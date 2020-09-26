39b188d4df496f1eb0e9942b40642a2dffb26fb6
Ike Ajah (RIT Student) <ira8296@rit.edu>
2020-09-25 22:50:53 -0400

    Finished setting up base for project

--- src/index.php
+++ src/index.php
@@ -1,10 +1,3 @@
-if (isset($_FILES['upload_file'])) {
-    if(move_uploaded_file($_FILES['upload_file']['tmp_name'], "datas/" . $_FILES['upload_file']['name'])){
-        echo $_FILES['upload_file']['name']. " OK";
-    } else {
-        echo $_FILES['upload_file']['name']. " KO";
-    }
-    exit;
-} else {
-    echo "No files uploaded ...";
-}
+<?php $ path = "files/"; $path = $path . basename( $_FILES['userfile']['name']);
+
+if(move_uploaded_file($_FILES['userfile']['tmp_name'], $path)) { echo "Success uploading". basename($_FILES['userfile']['name']); } else{ echo "Error when uploading file."; } ?>
